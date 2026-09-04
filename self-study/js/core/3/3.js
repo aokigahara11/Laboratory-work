15b19fe9d4c62282ac801e8bc1ab5ac77b93f0c9
aokigahara <nata.almukhametova.91@gmail.com>
2026-09-04 22:42:11 +0500

    refactor: delete blocks go and js for self-study

--- self-study/js/core/3/3.js
+++
@@ -1,8 +0,0 @@
-// Написать функцию mergeArrays(...arrays), которая принимает произвольное количество массивов 
-// через Rest-оператор (...) и объединяет их в один плоский массив.
-
-function mergeArrays(...arrays) {
-    const count = arrays.length()
-    let newArray = []
-    return newArray.concat(...arrays)
-}
--- self-study/js/core/4/1.js
+++
@@ -1,7 +0,0 @@
-// Задание 4.1: Дан массив чисел. С помощью метода .filter() 
-// отфильтровать только нечетные числа, а с помощью .map() возвести их в квадрат.
-
-function Filter(numbers) {
-    const result = numbers.filter(num => num % 2 == 0).map(num => num ** 2)
-    return result
-}
